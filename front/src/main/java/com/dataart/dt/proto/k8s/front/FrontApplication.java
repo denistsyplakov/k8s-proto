317a1ef8a27b055a436e425dafea4451ffca4d4a
Denis Tsyplakov <denis.tsyplakov@gmail.com>
2019-04-12 20:56:54 +0300

    step to config externalization

--- front/src/main/java/com/dataart/dt/proto/k8s/front/FrontApplication.java
+++ front/src/main/java/com/dataart/dt/proto/k8s/front/FrontApplication.java
@@ -1,10 +1,13 @@
 package com.dataart.dt.proto.k8s.front;
 
+import com.dataart.dt.proto.k8s.utility.ConfigExternalizationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,7 +15,8 @@ public class FrontApplication {
 
     static private Logger log = LoggerFactory.getLogger(FrontApplication.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ConfigExternalizationUtil.externalizeConfig();
         SpringApplication.run(FrontApplication.class, args);
     }
 
--- utility/src/main/java/com/dataart/dt/proto/k8s/utility/ConfigExternalizationUtil.java
+++ utility/src/main/java/com/dataart/dt/proto/k8s/utility/ConfigExternalizationUtil.java
@@ -1,23 +1,24 @@
 package com.dataart.dt.proto.k8s.utility;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.logging.Logger;
 
 public class ConfigExternalizationUtil {
 
-    private static Logger log = LoggerFactory.getLogger(ConfigExternalizationUtil.class);
+    private static Logger log = Logger.getLogger(ConfigExternalizationUtil.class.getName());
 
-    private static void externalizeConfig() throws IOException {
+    public static void externalizeConfig() throws IOException {
+        log.info("Trying to externalize config for a project started from dir: " +
+                new File(".").getAbsolutePath());
         var extConfig = new File("/config/application.properties");
         if (extConfig.exists()) {
-            log.info("Found externalized config file {}, copying it to /application.properties", extConfig.toPath());
+            log.info("Found externalized config file " + extConfig.toPath() +
+                    ", copying it to /application.properties");
             Files.copy(extConfig.toPath(), new File("/application.properties").toPath());
         } else {
-            log.info("Externalized config {} not found", extConfig.toPath());
+            log.info("Externalized config " + extConfig.toPath() + " not found" );
         }
     }
 
